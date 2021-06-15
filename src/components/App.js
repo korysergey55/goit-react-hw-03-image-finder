7e48b0f635333e6c1dc447845cf177199e2c73d6
Serhiy Korytskyy <korysergey55@gmail.com>
2021-06-15 17:44:20 +0300

    first-commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class App extends Component{
+    state = { 
+
+     }
+    render() {
+        return (
+            <>
+            <p>Hi</p>
+            </>
+        );
+    }
+}
+
+export default App;
